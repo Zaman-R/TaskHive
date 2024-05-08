a5043877201f83af0b361a9bdea36de86975579f
rifat.zaman <rifat.zaman@upaybd.com>
2024-05-08 12:33:40 +0600

    Code refactored

--- taskhive/main.py
+++ taskhive/main.py
@@ -1,10 +1,13 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, Path, HTTPException
 
-import models
+from sqlalchemy.orm import Session
+from starlette import status
+
+from models import Todo
 from database import engine, SessionLocal
+import models
 
 app = FastAPI()
 
@@ -19,6 +22,20 @@ def get_db():
         db.close()
 
 
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
 @app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):  # dependency injection , this fuctions relys on get_db()
-    return db.query(models.Todo).all()
+async def read_all(db: db_dependency):  # dependency injection , this fuctions relys on get_db()
+    return db.query(Todo).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todo).filter(Todo.id == todo_id).first()
+    print(todo_model)
+
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
+
